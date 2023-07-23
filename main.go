fd3d1259cd36fe3aa82867fb208e186d950b2ef6
teppei <teppei0830.4869@gmail.com>
2023-07-23 14:01:56 +0900

    main.go を修正しました。
    test_var/ パッケージをインポートして用いるようにしました。

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	"basic_grammer/test_pkg"
 	"fmt"
+
+	"github.com/ASAP-BYAF/go_pra/test_func"
+	"github.com/ASAP-BYAF/go_pra/test_pkg"
+	"github.com/ASAP-BYAF/go_pra/test_var"
 )
 
 func main() {
 	fmt.Println("hello world")
 	test_pkg.Pkg_func()
+	tmp := test_func.Func_func("Hello World")
+	fmt.Println(tmp)
+	test_var.Var_func()
 }

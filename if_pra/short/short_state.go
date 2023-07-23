0e1a9e2114352eefeb949c6672a5f7da08387f4d
teppei <teppei0830.4869@gmail.com>
2023-07-23 16:42:18 +0900

    新たに if_pra/short/ パッケージを追加しました。
    これは、 if 文の中で一時的な変数を作る練習です。

---
+++ if_pra/short/short_state.go
@@ -0,0 +1,9 @@
+package short
+
+func ShortState(x int) bool {
+	if y := x % 2; y == 0 {
+		return true
+	} else {
+		return false
+	}
+}
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/ASAP-BYAF/go_pra/for_test"
 	"github.com/ASAP-BYAF/go_pra/if_pra"
+	"github.com/ASAP-BYAF/go_pra/if_pra/short"
 	"github.com/ASAP-BYAF/go_pra/namedreturn"
 	"github.com/ASAP-BYAF/go_pra/pkg_func2"
 	"github.com/ASAP-BYAF/go_pra/pkg_multi_result"
@@ -20,30 +21,34 @@ func main() {
 	test_pkg.Pkg_func()
 
 	// 関数の定義
-	tmp := test_func.Func_func("Hello World")
-	fmt.Println(tmp)
+	tmp_str := test_func.Func_func("Hello World")
+	fmt.Println(tmp_str)
 
 	// 変数の定義
 	test_var.Var_func()
 
 	// 引数の型の省略
-	tmp2 := pkg_func2.Omit_func(3,2)
-	fmt.Println(tmp2)
+	tmp_int := pkg_func2.Omit_func(3,2)
+	fmt.Println(tmp_int)
 
 	// 複数の返り値を返す
-	tmp3, tmp4 := pkg_multi_result.MultiResultFunc(1.0, 2.0)
-    fmt.Println(tmp3, tmp4)
+	tmp_float, tmp_float2 := pkg_multi_result.MultiResultFunc(1.0, 2.0)
+    fmt.Println(tmp_float, tmp_float2)
 
 	// 返り値の名前を定義して返す
-	x, y := namedreturn.NamedReturn(2)
+	tmp_int2, tmp_int3 := namedreturn.NamedReturn(2)
 	// fmt.Println("x = ",x,",","y = ",y)
-	fmt.Printf("x = %d, y = %d\n",x,y)
+	fmt.Printf("x = %d, y = %d\n",tmp_int2, tmp_int3)
 
 	// practice of for loop
-	z := for_test.ForLoop()
-	fmt.Printf("z = %d\n", z)
+	tmp_int = for_test.ForLoop()
+	fmt.Printf("z = %d\n", tmp_int)
 
 	// practice of if
-	flag := if_pra.IfPra(2.0)
-	fmt.Printf("flag = %v\n", flag)
+	tmp_flag := if_pra.IfPra(2.0)
+	fmt.Printf("flag = %v\n", tmp_flag)
+
+	// practice of if with a short statement
+	tmp_flag = short.ShortState(3)
+	fmt.Printf("flag = %v\n", tmp_flag)	
 }

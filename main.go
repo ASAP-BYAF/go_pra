befd9a58659a90a17edc4a76d5b3b479e7fb53bf
teppei <teppei0830.4869@gmail.com>
2023-07-23 14:23:00 +0900

    新たに pkg_func2/ パッケージを追加しました。
    これは、関数の引数が複数あり、その方が同じ場合は
    型の指定を省略できる機能を試すために作成しました。

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/ASAP-BYAF/go_pra/pkg_func2"
 	"github.com/ASAP-BYAF/go_pra/test_func"
 	"github.com/ASAP-BYAF/go_pra/test_pkg"
 	"github.com/ASAP-BYAF/go_pra/test_var"
@@ -14,4 +15,6 @@ func main() {
 	tmp := test_func.Func_func("Hello World")
 	fmt.Println(tmp)
 	test_var.Var_func()
+	tmp2 := pkg_func2.Omit_func(3,2)
+	fmt.Println(tmp2)
 }
---
+++ pkg_func2/omit_type.go
@@ -0,0 +1,5 @@
+package pkg_func2
+
+func Omit_func(x, y int) int {
+	return x * y
+}
